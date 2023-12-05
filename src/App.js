399cb0b6ce82974188e80575f6aa619de3dd186d
sandertje <o.martian@unkd.com.ua>
2023-12-05 21:08:20 +0200

    feat: add slider and first slide

--- src/App.js
+++ src/App.js
@@ -1,44 +1,30 @@
-import FirstWindow from './components/FirstWindow/FirstWindow';
-import SecondWindow from './components/SecondWindow/SecondWindow';
-import ThirdWindow from './components/ThirdWindow/ThirdWindow';
-import Header from './components/Header/Header';
-import styles from "./App.module.scss";
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
+import './styles/styles.scss';
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
-
-import './styles/styles.scss';
-
-// import required modules
 import { Mousewheel, Pagination } from 'swiper/modules';
+import FirstWindow from './components/FirstWindow/FirstWindow';
 
 function App() {
-
   return (
     <>
       <Swiper
-        // direction={'vertical'}
+        direction={'vertical'}
         slidesPerView={1}
-        // mousewheel={true}
+        spaceBetween={30}
+        mousewheel={true}
         pagination={{
           clickable: true,
         }}
         modules={[Mousewheel, Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>
+          <FirstWindow/>
+        </SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
       </Swiper>
     </>
   );
